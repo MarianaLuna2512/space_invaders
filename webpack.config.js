b60a241c511403093f25d966fd002d443b6b990a
Angela Mariana Correa Luna <amcorreal@libertadores.edu.co>
2026-02-28 21:37:09 +0000

    Cambios personaje 1 y2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
-module.exports = {
-  entry: "./lib/space_invaders.js",
-  output: {
-  	filename: "./bundle.js"
-  },
-  devtool: 'source-map'
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+
+  const config = {
+    mode,
+    entry: "./lib/space_invaders.js",
+    output: {
+      filename: "./bundle.js"
+    },
+    devtool: mode === 'development' ? 'source-map' : false
+  };
+
+  if (mode === 'development') {
+    config.devServer = {
+      static: {
+        directory: __dirname,
+      },
+      port: 8080,
+      open: true,
+      hot: true
+    };
+  }
+
+  return config;
 };
